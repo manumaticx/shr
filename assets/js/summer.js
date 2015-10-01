b75140144bf41c7f03df7a0fac36cedb180091d3
Manuel Lehner <manumaticx@gmail.com>
2015-10-01 23:15:00 +0200

    update index

--- assets/js/summer.js
+++ assets/js/summer.js
@@ -10,20 +10,6 @@ var summer = (function ($) {
         postBgImages = '.bg-img img',
         postCoverImg = '.summer-post-header .bg-img',
 
-    // post animations on homepage
-    indexPostAnimate = function () {
-        if ($(indexPostClass).length) {
-            $(indexPostClass).each(function () {
-            var postPos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop(),
-                windowHeight = $(window).height();
-                if (postPos < topOfWindow + (windowHeight/ 1.4)) {
-                    $(this).addClass('fadeInDown');
-                }
-            });
-        }
-    },
-
     mobileMenu = function () {
         if($(mainMenu).length) {
             $(mobileMenuButton).on('click', function(e){
@@ -46,24 +32,10 @@ var summer = (function ($) {
         }
     },
 
-    postHeaderCoverImg = function () {
-        var coverImage = $('[alt=cover-image]');
-        if (coverImage.length) {
-            $(postCoverImg).append('<img src="' + coverImage.attr('src') + '">');
-            coverImage.remove();
-        }
-    },
-
     // summer javascripts initialization
     init = function () {
-        indexPostAnimate();
-        $(window).on('scroll', function() {
-            indexPostAnimate();
-        });
-        postHeaderCoverImg();
         mobileMenu();
         headerTitlesBackgroundCheck();
-        $('p:has(> img)').addClass('with-image');
     };
 
     return {
